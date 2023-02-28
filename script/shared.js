38c11f571c93be88ae6157a565154b04889ca500
rokeppen <robinkep@gmail.com>
2023-02-28 23:09:48 +0100

    Weekend info test

--- script/shared.js
+++ script/shared.js
@@ -240,3 +240,15 @@ function mailto(location) {
             break
     }
 }
+
+function downloadPdf() {
+    var element = document.getElementById('content');
+    var opt = {
+        margin:       1,
+        filename:     'myfile.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 2 },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+    html2pdf().set(opt).from(element).save();
+}
