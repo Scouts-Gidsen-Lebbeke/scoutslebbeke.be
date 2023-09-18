deb94f259c39d1eb376c2815a729e5730ad25bc6
rokeppen <robinkep@gmail.com>
2023-09-18 20:34:46 +0200

    Subsidie API (being lazy)

--- api/getCalendarByPeriod.php
+++ api/getCalendarByPeriod.php
@@ -4,22 +4,51 @@ $result = array();
 $from = parse(@$_GET['from']);
 $to = parse(@$_GET['to']);
 if ($from == null && $to == null) {
-    $sql = "select * from `calendar-item` order by fromDate";
+    $sql = "from `calendar-item`";
 } elseif ($from == null) {
-    $sql = "select * from `calendar-item` where `toDate` < FROM_UNIXTIME('$to') order by fromDate";
+    $sql = "from `calendar-item` where toDate < FROM_UNIXTIME('$to')";
 } elseif ($to == null) {
-    $sql = "select * from `calendar-item` where `fromDate` > FROM_UNIXTIME('$from') order by fromDate";
+    $sql = "from `calendar-item` where fromDate > FROM_UNIXTIME('$from')";
 } else {
-    $sql = "select * from `calendar-item` where `fromDate` > FROM_UNIXTIME('$from') and `toDate` < FROM_UNIXTIME('$to') order by fromDate";
+    $sql = "from `calendar-item` where fromDate > FROM_UNIXTIME('$from') and toDate < FROM_UNIXTIME('$to')";
 }
+$select = "select *";
+if (@$_GET["grouped"] == "true") {
+    $sql = $sql." group by fromDate, toDate";
+    $select = $select.", count(*) as sum";
+}
+$sql = $select." ".$sql." order by fromDate";
+$totalDiff = 0;
 if ($query = $connection->query($sql)) {
     while ($row = $query->fetch_array(MYSQLI_ASSOC)) {
-        array_push($result, $row["fromDate"]." tot ".$row["toDate"].": ".$row["title"]." (".toBranch($row["group"]).")");
+        if (key_exists("sum", $row) && $row["sum"] != "1") {
+            $groups = intval($row["sum"]);
+            $title = "Zondagse werking";
+        } else {
+            $groups = 1;
+            $title = $row["title"];
+        }
+        if (@$_GET["grouped"] == "true") {
+            $sql = $sql." group by fromDate, toDate";
+            $select = $select.", count(*) as sum";
+            $fromDate = new DateTime(date('Y-m-d H:i:s', parse($row["fromDate"])));
+            $toDate = new DateTime(date('Y-m-d H:i:s', parse($row["toDate"])));
+            $diff = (intval(($toDate->diff($fromDate))->format('%a')) + 1) * $groups;
+            $extra = $fromDate != null && $toDate != null ? " => ".$diff." werkingen" : "";
+        } else {
+            $extra = "";
+            $diff = 0;
+        }
+        $totalDiff += $diff;
+        array_push($result, $row["fromDate"]." tot ".$row["toDate"].": ".$title." (".toBranches($groups).")".$extra);
     }
 }
 $query->close();
 $connection->close();
-echo join("<br>", $result);
+echo join("<br/>", $result);
+if ($totalDiff > 0) {
+    echo "<br/><b>=> ".$totalDiff." werkingen in totaal = ".($totalDiff/5)." werkingsdagen</b>";
+}
 
 function parse($date): int|null {
     if ($date == null) return null;
@@ -27,14 +56,9 @@ function parse($date): int|null {
     return strtotime($date);
 }
 
-function toBranch($branch): string {
-    return match (intval($branch)) {
-        1 => "Kapoenen",
-        2 => "Welpen",
-        3 => "Pioniers",
-        4 => "Jonggivers",
-        5 => "Givers",
-        6 => "Jins",
-        default => "Onbepaald",
+function toBranches($count): string {
+    return match ($count) {
+        1 => "1 groep",
+        default => $count . " groepen",
     };
 }
