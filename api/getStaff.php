538458a033e95aefa968f593312e6168d7138ae1
rokeppen <robinkep@gmail.com>
2023-10-02 00:04:25 +0200

    Add old staff

--- api/getStaff.php
+++ api/getStaff.php
@@ -4,7 +4,7 @@ $tak = $_GET['q'];
 $result = array();
 if ($q = $connection->query("select setting_value from settings where setting_id='staff_visible'")) {
     if ((strcmp($tak, 'Stam') === 0 || filter_var(mysqli_fetch_assoc($q)['setting_value'], FILTER_VALIDATE_BOOLEAN)) &&
-        $query = $connection->query("select Voornaam, Achternaam, kapoenenbijnaam, welpenbijnaam, Totem, Gsm, (select default_email from functions where function=Functie) as Email, Foto, Takleiding from staff where Functie='$tak' order by Takleiding, Voornaam desc")) {
+        $query = $connection->query("select Voornaam, Achternaam, kapoenenbijnaam, welpenbijnaam, Totem, Gsm, (select default_email from functions where function=Functie) as Email, Foto, Takleiding from staff where Functie='$tak' order by Takleiding desc, Voornaam")) {
         while ($row = $query->fetch_array(MYSQLI_ASSOC)) {
             array_push($result, $row);
         }
