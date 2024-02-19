b55ebeab88c221239158bd5ca4dc74e1c8fd419e
rokeppen <robinkep@gmail.com>
2024-02-19 23:07:44 +0100

    Retrieve all event info

--- api/getEvent.php
+++ api/getEvent.php
@@ -2,5 +2,7 @@
 require "connect.php";
 $event_id = $_GET['id'];
 $event = mysqli_fetch_object($connection->query("select * from event where id='$event_id'"));
+$event->location = mysqli_fetch_object($connection->query("select * from location where id='$event->location_id'"));
+$event->restrictions = mysqli_all_objects($connection, "select * from event_restriction where id='$event_id'");
 $connection->close();
 echo json_encode($event);
