086bcc8c7fa6243eba267187345947265448d866
rokeppen <robinkep@gmail.com>
2024-02-19 23:05:32 +0100

    Cleanup backoffice and image directory

--- backoffice/api/session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['user'])) {
-    session_destroy();
-    echo json_encode(array("active" => false));
-} else {
-    echo json_encode(array("active" => true, "session" => $_SESSION));
-}
