41db4122c7c673705589a56aa81b978566170888
rokeppen <robinkep@gmail.com>
2024-03-01 01:06:06 +0100

    Only add default background to git

--- api/getBackgrounds.php
+++ api/getBackgrounds.php
@@ -1,6 +1,6 @@
 <?php
-$dir = $_SERVER['DOCUMENT_ROOT'] . '/background';
+$dir = $_SERVER['DOCUMENT_ROOT'] . '/images/background';
 echo json_encode(array_filter(scandir($dir), function($i) {
     global $dir;
-    return $i !== ".." && $i !== "." && !is_dir($dir . $i); // is_dir not working with webhost PHP version
+    return $i !== ".." && $i !== "." && !is_dir($dir . $i);
 }));
