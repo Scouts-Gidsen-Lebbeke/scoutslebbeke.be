e47c4092dba174cba1861540e7f17092c7ca5bd3
rokeppen <robinkep@gmail.com>
2024-03-01 01:21:00 +0100

    Move backgrounds and only track location server side

--- api/getBackgrounds.php
+++ api/getBackgrounds.php
@@ -1,6 +1,6 @@
 <?php
 $dir = $_SERVER['DOCUMENT_ROOT'] . '/images/background';
-echo json_encode(array_filter(scandir($dir), function($i) {
+echo json_encode(array_map(fn($i) => '/images/background/' . $i, array_filter(scandir($dir), function($i) {
     global $dir;
     return $i !== ".." && $i !== "." && !is_dir($dir . $i);
-}));
+})));
