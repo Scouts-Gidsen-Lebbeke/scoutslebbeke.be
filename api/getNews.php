d486470278fd690a4f3f643f3fb342625b950001
rokeppen <robinkep@gmail.com>
2024-03-01 01:17:58 +0100

    Fix news images

--- api/getNews.php
+++ api/getNews.php
@@ -1,7 +1,7 @@
 <?php
 include "connect.php";
 $result = array();
-if ($query = $connection->query("select * from news n left join user l on n.user_id = l.id where visible order by date desc")) {
+if ($query = $connection->query("select title, content, n.image, date, l.name, l.first_name from news n left join user l on n.user_id = l.id where visible order by date desc")) {
     while ($row = $query->fetch_array(MYSQLI_ASSOC)) {
         array_push($result, $row);
     }
