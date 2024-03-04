0bce64891fb3c12f65081c448c12c01425c3fc40
rokeppen <robinkep@gmail.com>
2024-03-04 21:17:07 +0100

    Don't rely on the global variable

--- api/getSubscriptionState.php
+++ api/getSubscriptionState.php
@@ -2,6 +2,6 @@
 require 'getInternalLogin.php';
 
 $event_id = $_GET['id'];
-$user = fetchUser($sgl_id);
+$user = getUser(true);
 $registration = mysqli_fetch_object($connection->query("select * from event_registration where user_id = '$user->id' and event_id = '$event_id' order by date desc limit 1"));
 echo json_encode($registration);
