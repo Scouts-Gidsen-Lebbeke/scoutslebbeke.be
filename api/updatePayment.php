e7aba3ff43b6517ccb65bfe7eb53cc03f5e273ed
rokeppen <robinkep@gmail.com>
2024-03-07 00:10:07 +0100

    Use .env for cc

--- api/updatePayment.php
+++ api/updatePayment.php
@@ -14,9 +14,9 @@ try {
         $user_id = $payment->metadata->user_id;
         $user = mysqli_fetch_object($connection->query("select * from user where id = '$user_id'"));
         $mail->addAddress($user->email);
-        $mail->addCC('info@scoutslebbeke.be');
+        $mail->addCC($config["MAIL_FROM_ADDRESS"]);
         $mail->isHTML();
-        $mail->Subject = "Inschrijvingsbevestiging '$payment->description'";
+        $mail->Subject = "Inschrijvingsbevestiging $payment->description";
         $mail->Body = "<p>Dag $user->first_name,</p><p>We hebben de betaling goed ontvangen, dus hierbij bevestigen we jouw inschrijving voor $payment->description. Tot dan!</p><p>Stevige linker,<br/>De leiding</p>";
         $mail->AltBody = "Dag $user->first_name, we hebben de betaling goed ontvangen, dus hierbij bevestigen we jouw inschrijving voor $payment->description. Tot dan! Stevige linker, de leiding";
         $mail->send();
