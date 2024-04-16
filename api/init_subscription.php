4b32088e832a32ad532ec79c0c51fb00152dabc8
rokeppen <robinkep@gmail.com>
2024-04-16 22:20:55 +0200

    Complete separation between activities and events

--- api/init_subscription.php
+++ api/init_subscription.php
@@ -10,7 +10,7 @@ function getSubscriptionState($user): stdClass {
             throw new InvalidArgumentException("Jouw medische fiche is al meer dan een jaar niet aangevuld, 
             update deze eerst in de <a href='https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/frontend/lid/individuelesteekkaart/$user->sgl_id' target='_blank' style='color: red'>Groepsadministratie</a>!");
         }
-        $result->registration = mysqli_fetch_object($connection->query("select * from activity_registration where user_id = '$user->id' and event_id = '$activity_id' and payment_id is not null and status not in ('canceled', 'expired', 'failed') order by date desc limit 1"));
+        $result->registration = mysqli_fetch_object($connection->query("select * from activity_registration where user_id = '$user->id' and activity_id = '$activity_id' and payment_id is not null and status not in ('canceled', 'expired', 'failed') order by date desc limit 1"));
         if ($result->registration == null) {
             $activity = mysqli_fetch_object($connection->query("select * from activity where id = '$activity_id'"));
             if ($activity == null) {
