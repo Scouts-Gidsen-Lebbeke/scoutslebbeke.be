926d904e426d77e7cc78e04bb6a99f8d01303734
rokeppen <robinkep@gmail.com>
2024-04-16 21:45:45 +0200

    String delimiter and interpolation consistency, retrieve pages from root on .html presence to support subdirectories

--- script/shared.js
+++ script/shared.js
@@ -3,56 +3,47 @@ let currentIndex = 0, intervalID, backgrounds = [], kc;
 function loadGlobal() {
     getImages();
     fetch("/api/getNavigation.php").then((res) => res.json()).then((data) => {
-        $('#navigation').html(getBrowserNav(data));
+        $("#navigation").html(getBrowserNav(data));
         $("#mobile-navigation").html(getMobileNav(data));
     });
-    $('#current-year').text(new Date().getFullYear())
+    $("#current-year").text(new Date().getFullYear());
 }
 
 function load(page) {
-    window.location = page.includes(".html") ? page : `/${page}.html`;
+    window.location = page.includes(".html") ? `/${page}` : `/${page}.html`;
 }
 
 function getImages() {
-    fetch(new Request('/api/getBackgrounds.php')).then(response => response.json()).then(data => {
+    fetch("/api/getBackgrounds.php").then(response => response.json()).then(data => {
         backgrounds = Object.values(data);
         backgrounds.forEach(url => {
             const img = new Image();
             img.src = url;
         })
-        resetAndChangeImage()
+        resetAndChangeImage();
     })
 }
 
 function resetAndChangeImage() {
     clearInterval(intervalID);
-    changeImage()
+    changeImage();
     intervalID = setInterval(changeImage, 5000);
 }
 
 function changeImage() {
-    currentIndex = (currentIndex + 1) % backgrounds.length
-    $("#title-wrapper").css("background-image", "url(" + backgrounds[currentIndex] + ")");
+    currentIndex = (currentIndex + 1) % backgrounds.length;
+    $("#title-wrapper").css("background-image", `url(${backgrounds[currentIndex]})`);
 }
 
 function toggleNav() {
-    let mobileNav = $("#mobile-navigation")
-    mobileNav.toggle()
-    $("body").css({"overflow": mobileNav.is(":visible") ? "hidden" : "visible"})
-}
-
-function closeNav() {
-    let mobileNav = $("#mobile-navigation")
-    if (mobileNav.is(":visible")) {
-        mobileNav.hide()
-        $("body").css({"overflow": "visible"})
-        $('.burger').toggleClass('opened')
-    }
+    let mobileNav = $("#mobile-navigation");
+    mobileNav.toggle();
+    $("body").css({"overflow": mobileNav.is(":visible") ? "hidden" : "visible"});
 }
 
 function toggleSub(sub) {
-    $('#' + sub + '-mobile-menu').toggle()
-    $("#main-mobile-menu").toggle()
+    $(`#${sub}-mobile-menu`).toggle();
+    $("#main-mobile-menu").toggle();
 }
 
 function getBrowserNav(groups) {
@@ -60,11 +51,11 @@ function getBrowserNav(groups) {
     groups.forEach((group) => {
         menuitem = "";
         if (group.items.length === 1 && group.items[0].name === group.name) {
-            menuitem += "<div class='navigation-item' onclick=\"load('" + group.items[0].path + "');\">" + group.name + "</div>";
+            menuitem += `<div class="navigation-item" onclick="load('${group.items[0].path}')">${group.name}</div>`;
         } else if (group.items.length > 0) {
-            menuitem += "<div class='dropdown-block'><div class=\"navigation-item\">" + group.name + " <i class=\"down\"></i></div><div class=\"dropdown\">";
+            menuitem += `<div class="dropdown-block"><div class="navigation-item">${group.name} <i class="down"></i></div><div class="dropdown">`;
             group.items.forEach((item) => {
-                menuitem += "<div class='dropdown-item' onclick=\"load('" + item.path + "')\">" + item.name + "</div>";
+                menuitem += `<div class="dropdown-item" onclick="load('${item.path}')">${item.name}</div>`;
             });
             menuitem += "</div></div>";
         }
@@ -77,49 +68,55 @@ function getMobileNav(groups) {
     let subitems = '', result = '';
     groups.forEach((group) => {
         if (group.items.length === 1 && group.items[0].name === group.name) {
-            subitems += `<a onclick="load('${group.items[0].path}')\">${group.name}</a>`;
+            subitems += `<a onclick="load('${group.items[0].path}')">${group.name}</a>`;
         } else if (group.items.length > 0) {
             subitems += `<a onclick="toggleSub('${group.id}')">${group.name}</a>`;
-            result += `<div class='mobile-menu' id='${group.id}-mobile-menu'>`;
+            result += `<div class="mobile-menu" id="${group.id}-mobile-menu">`;
             group.items.forEach((item) => {
                 result += `<a onclick="load('${item.path}')">${item.name}</a>`;
             });
-            result += `<a onclick="toggleSub('${group.id}')">←</a></div>`
+            result += `<a onclick="toggleSub('${group.id}')">←</a></div>`;
         }
     });
-    return `<div class='mobile-menu' id='main-mobile-menu'>${subitems}</div>${result}`;
+    return `<div class="mobile-menu" id="main-mobile-menu">${subitems}</div>${result}`;
 }
 
 function mailto(location) {
     switch (location) {
         case "webmaster":
-            window.location.href = atob("bWFpbHRvOndlYm1hc3RlckBzY291dHNsZWJiZWtlLmJl")
-            break
+            window.location.href = atob("bWFpbHRvOndlYm1hc3RlckBzY291dHNsZWJiZWtlLmJl");
+            break;
         case "info":
-            window.location.href = atob("bWFpbHRvOmluZm9Ac2NvdXRzbGViYmVrZS5iZQ==")
-            break
+            window.location.href = atob("bWFpbHRvOmluZm9Ac2NvdXRzbGViYmVrZS5iZQ==");
+            break;
         case "vzw":
-            window.location.href = atob("bWFpbHRvOnZ6d0BzY291dHNsZWJiZWtlLmJl")
-            break
+            window.location.href = atob("bWFpbHRvOnZ6d0BzY291dHNsZWJiZWtlLmJl");
+            break;
     }
 }
 
-async function tokenized(url) {
-    await kc.updateToken(30)
+async function tokenized(url, optional = false) {
+    if (!kc.token && optional) {
+        return fetch(url).then(data => data.json());
+    }
+    await kc.updateToken(30);
     if (kc.token) {
         return fetch(url, {
             headers: new Headers({ 'Authorization': `Bearer ${kc.token}` })
-        }).then(data => data.json())
+        }).then(data => data.json());
     }
-    return null
+    if (optional) {
+        return fetch(url).then(data => data.json());
+    }
+    return null;
 }
 
 function toggleLogin() {
-    kc.login({ redirectUri: document.location })
+    kc.login({ redirectUri: document.location });
 }
 
 async function loadKeycloak() {
-    kc = new Keycloak("/script/keycloak.json")
+    kc = new Keycloak("/script/keycloak.json");
     return kc.init({
         onLoad: 'check-sso',
         silentCheckSsoFallback: false,
@@ -130,40 +127,40 @@ async function loadKeycloak() {
 async function checkLogin(onFulfilled) {
     const authenticated = await loadKeycloak();
     if (authenticated) {
-        tokenized("/api/getLogin.php").then(onFulfilled)
+        tokenized("/api/getLogin.php").then(onFulfilled);
     }
 }
 
 async function requireLogin(onFulfilled) {
     const authenticated = await loadKeycloak();
     if (authenticated) {
-        tokenized("/api/getLogin.php").then(onFulfilled)
+        tokenized("/api/getLogin.php").then(onFulfilled);
     } else {
         toggleLogin();
     }
 }
 
 function loadProfile(d) {
-    $('#profile-name').text(d.first_name)
-    $('#profile-pic').css("background-image", "url(/images/profile/" + d.image + ")")
+    $("#profile-name").text(d.first_name);
+    $("#profile-pic").css("background-image", `url(/images/profile/${d.image})`);
     $("#profile-dropdown-block").click(function(){
-        window.location = "/profile.html"
+        window.location = "/profile.html";
     });
 }
 
 function periodToTitle(from, to) {
     if (from.getDate() === to.getDate()) {
-        return capitalize(`${printDate(from)}, ${printTime(from)} - ${printTime(to)}`)
+        return capitalize(`${printDate(from)}, ${printTime(from)} - ${printTime(to)}`);
     }
-    return capitalize(`${printDate(from)}, ${printTime(from)} - ${printDate(to)}, ${printTime(to)}`)
+    return capitalize(`${printDate(from)}, ${printTime(from)} - ${printDate(to)}, ${printTime(to)}`);
 }
 
 function printDate(date) {
-    return date.toLocaleDateString('nl-BE', { weekday: 'short', month: 'numeric', day: 'numeric' })
+    return date.toLocaleDateString('nl-BE', { weekday: 'short', month: 'numeric', day: 'numeric' });
 }
 
 function printTime(date) {
-    return date.toLocaleTimeString('nl-BE', { hour: '2-digit', minute:'2-digit' })
+    return date.toLocaleTimeString('nl-BE', { hour: '2-digit', minute: '2-digit' });
 }
 
 function locationToTitle(location, full) {
@@ -181,7 +178,7 @@ function locationToTitle(location, full) {
 }
 
 function capitalize(s) {
-    return s.charAt(0).toUpperCase() + s.slice(1)
+    return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
 function ifNotNull(i, orElse = "") {
