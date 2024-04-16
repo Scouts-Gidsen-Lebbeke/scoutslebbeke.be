091f0c58dde458137f831502095838fcf052b50c
rokeppen <robinkep@gmail.com>
2024-04-16 22:35:35 +0200

    Complete separation between activities and events

--- api/getNavigation.php
+++ api/getNavigation.php
@@ -4,15 +4,15 @@ $groups = mysqli_all_objects($connection, "select * from page_groups order by `r
 foreach ($groups as $group) {
     if ($group->name == "Activiteiten") {
         $group->items = array();
-        foreach (mysqli_all_objects($connection, "select * from activity where now() < end order by start") as $event) {
-            $event_page = (object) [
-                "name" => $event->name,
-                "path" => "activity.html?id=" . $event->id,
-                "rank" => $event->start,
+        foreach (mysqli_all_objects($connection, "select * from activity where now() < end order by start") as $activity) {
+            $activity_page = (object) [
+                "name" => $activity->name,
+                "path" => "activity.html?id=" . $activity->id,
+                "rank" => $activity->start,
                 "visible" => true,
                 "group_id" => $group->id
             ];
-            $group->items[] = $event_page;
+            $group->items[] = $activity_page;
         }
     } else if ($group->name == "Takken") {
         $group->items = array();
