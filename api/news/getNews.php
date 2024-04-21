e3699eac8e3965f4eae215520f67d6ad86918636
rokeppen <robinkep@gmail.com>
2024-04-22 01:10:25 +0200

    News fix

--- api/news/getNews.php
+++ api/news/getNews.php
@@ -1,5 +1,5 @@
 <?php
-include "connect.php";
+include "../connect.php";
 $result = array();
 if ($query = $connection->query("select title, content, n.image, date, l.name, l.first_name from news n left join user l on n.user_id = l.id where visible order by date desc")) {
     while ($row = $query->fetch_array(MYSQLI_ASSOC)) {
