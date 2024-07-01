654f0edb174a83ce02efa74b9a126a391e41a2f4
rokeppen <robinkep@gmail.com>
2024-07-01 15:08:41 +0200

    Only show paid registrations

--- api/activity/getActivityOverview.php
+++ api/activity/getActivityOverview.php
@@ -3,10 +3,9 @@ require '../getInternalLogin.php';
 
 guardStaff();
 $id = $_GET["id"];
-$branch = $_GET["branch"];
 $result = new stdClass();
 $result->activity = mysqli_fetch_object($connection->query("select * from activity where id = '$id'"));
-$result->registrations = mysqli_all_objects($connection, $query = "select * from activity_registration where activity_id = '$id'");
+$result->registrations = mysqli_all_objects($connection, $query = "select * from activity_registration where activity_id = '$id' and status='paid'");
 foreach ($result->registrations as $r) {
     $r->user = fetchUserById($r->user_id);
 }
