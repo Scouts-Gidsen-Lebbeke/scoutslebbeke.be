72bcbe0081c605f184a3804f8d32ec265b0f4348
rokeppen <robinkep@gmail.com>
2024-08-12 23:04:15 +0200

    Try medical icons

--- staff/members.js
+++ staff/members.js
@@ -27,7 +27,7 @@ function loadMembers() {
                     <td>${member.name}</td>
                     <td><a onclick="mailto:${member.email}">${member.email}</a></td>
                     <td><a onclick="callto:${member.mobile}">${member.mobile}</td>
-                    <td>${member.membership_date}</td>
+                    <td>${printDDMMYYYY(member.membership_date)}</td>
                     <td class="multi-icon-column">${retrieveExtraIcons(member)}</td>
                     <td><img src="/images/${member.medical_attention ? 'cross-red' : 'cross'}.png" class="subscription-icon" alt="pill" onclick="showMedicalOverview('${member.sgl_id}')"></td>
                     <td><img src="/images/report.png" class="subscription-icon" title="Download inschrijvingsbewijs" alt="report" onclick="getCertificate('${member.id}')"></td>
