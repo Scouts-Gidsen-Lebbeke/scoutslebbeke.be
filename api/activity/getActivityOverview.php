6c7f7f0e2ab40903ebe2e3fa918dc6371b4fadbb
rokeppen <robinkep@gmail.com>
2024-08-20 23:08:41 +0200

    Event registrations should only be visible if paid

--- api/activity/getActivityOverview.php
+++ api/activity/getActivityOverview.php
@@ -5,7 +5,7 @@ guardStaff();
 $id = $_GET["id"];
 $result = new stdClass();
 $result->activity = mysqli_fetch_object($connection->query("select * from activity where id = '$id'"));
-$result->registrations = mysqli_all_objects($connection, $query = "select * from activity_registration where activity_id = '$id' and status='paid'");
+$result->registrations = mysqli_all_objects($connection, $query = "select * from activity_registration where activity_id = '$id' and status = 'paid'");
 foreach ($result->registrations as $r) {
     $r->user = fetchUserById($r->user_id);
 }
--- api/event/getEventOverview.php
+++ api/event/getEventOverview.php
@@ -3,6 +3,6 @@ require '../getInternalLogin.php';
 
 guardStaff();
 $id = $_GET["id"];
-$result = mysqli_all_objects($connection, "select * from event_registration where event_id = '$id' order by date");
+$result = mysqli_all_objects($connection, "select * from event_registration where event_id = '$id' and status = 'paid' order by date");
 echo json_encode($result);
 $connection->close();
