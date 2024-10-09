052a78d67a842f60de4d12d29eba9f909f6dd8f5
rokeppen <robinkep@gmail.com>
2024-10-10 01:44:25 +0200

    Empty env vars don't trigger ??

--- api/init_env.php
+++ api/init_env.php
@@ -3,7 +3,10 @@ $config = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/.env');
 $EXTERNAL_ORGANIZATION_ID = $config["ORGANIZATION_ID"];
 $debug = filter_var($config["APP_DEBUG"], FILTER_VALIDATE_BOOLEAN);
 $APP_ENV = $config["APP_ENV"];
-$GA_API = $config["EXPLICIT_GA_API"] ?? match ($APP_ENV) {
+$GA_API = match ($APP_ENV) {
     "production" => "https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga/",
     default => "https://ga-staging.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga/",
 };
+if (!empty($config["EXPLICIT_GA_API"])) {
+    $GA_API = $config["EXPLICIT_GA_API"];
+}
